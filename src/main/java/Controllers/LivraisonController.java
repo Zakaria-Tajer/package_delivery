af08c6818502276cfb849d5b01d95af8773694e4
Zakaria-Tajer <za.tajer@gmail.com>
2022-11-28 16:19:04 +0100

    livraison page

---
+++ src/main/java/Controllers/LivraisonController.java
@@ -0,0 +1,20 @@
+package Controllers;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "LivraisonController", value = "/LivraisonController")
+public class LivraisonController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
